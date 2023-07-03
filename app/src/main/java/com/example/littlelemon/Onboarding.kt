80cddfffaedc5b4f0281a505c549990fd02a2731
Rishabh Rathi <72991781+RishabhRathi-Dev@users.noreply.github.com>
2023-07-03 13:15:03 +0530

    logo scaled

--- app/src/main/java/com/example/littlelemon/Onboarding.kt
+++ app/src/main/java/com/example/littlelemon/Onboarding.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.Shape
@@ -36,7 +37,8 @@ fun Onboarding(){
         Image(painter = painterResource(id = R.drawable.logo),
             contentDescription = "Logo",
             modifier = Modifier
-                .size(150.dp)
+                .size(width = 150.dp, height = 100.dp)
+                .scale(1.5f)
         )
 
         Row(
